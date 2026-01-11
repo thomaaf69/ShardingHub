8842ca851d4e1a625cd6e57c4de197975367a5f2
Thoma <98554519+thomaaf69@users.noreply.github.com>
2026-01-11 12:18:11 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/shardinghub/main.go
@@ -0,0 +1,23 @@
+// cmd/shardinghub/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "shardinghub/internal/shardinghub"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := shardinghub.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
